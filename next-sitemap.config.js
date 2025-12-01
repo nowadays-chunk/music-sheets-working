94d198b0427f480066af4eaa297f7e79a1d75b2e
The Well Guided <h.eljaouhari.home@gmail.com>
2025-12-01 13:36:41 +0100

    great

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://strum.fun', // Replace with your actual site URL
+    siteUrl: 'https://sheets.media', // Replace with your actual site URL
     generateRobotsTxt: true, // Generate a robots.txt file
   };
   
